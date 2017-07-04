91d30e194f0c442c27425a83f87ef119e08d1695
Cemmsaka <cemmsaka@hotmail.com>
2017-07-04 12:16:30 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,115 +1,50 @@
 <?php
-/* Main page with two forms: sign up and log in */
-include_once 'database.php';
-new mydatabase;
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sign-Up/Login Form</title>
-  <?php include 'css/css.html'; ?>
-  <link rel="shortcut icon" href="img/logo.png" type="image/x-icon"/>
-</head>
 
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    if (isset($_POST['login'])) { //logging in
-      require 'login.php';
+print_r(PDO::getAvailableDrivers());
 
-    }
+$dbHost = "localhost";
+$database = "2";
+$user = "Cem";
+$wachtwoord = "kurwa123";
 
-    elseif (isset($_POST['register'])) { //user registering
+try {
+    $conn = new PDO("mysql:host=$dbHost;dbname=$database", $user, $wachtwoord);
+    $conn ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "<br>Verbinding is gemaakt<br>";
+} catch (PDOException $e) {
+    $message = $e ->getMessage();
+    echo "<br>$message<br>";
+}
+// $query = "INSERT INTO testusers (user,wachtwoord) VALUES ( ? , ? );";
+//
+// $insert = $conn ->prepare($query);
+//
+// $data = array("cem","cem");
+//
+// try {
+//   $insert ->execute($data);
+// echo "gebruiker aangemaakt";
+// } catch (PDOException $e) {
+//   echo $e->getMessage();
+//   echo "gebruiker niet aangemaakt";
+$query = "SELECT * FROM testusers WHERE USER= :user";
 
-        require 'register.php';
-      }
+$data = "Cem";
+
+$SELECT = $conn->prepare($query);
+// bindparameter moet by reference dus via variable
+//alternatief is $select->bindValue("user", "testuser", PDO::PARAM_STR);
+try {
+  $SELECT->bindParam(":user", $data, PDO::PARAM_STR);
+  $SELECT->execute();
+  $result = $SELECT->fetchAll();
+// resultaat naar t scherm printen
+echo "id \t user \t wachtwoord";
+foreach ($result as $item) {
+  echo "<li>{$item["id"]} {$item["user"]} {$item["wachtwoord"]} </li>";
+}
+
+}catch(PDOException $e){
+
 }
 ?>
-
-  <div class="form">
-
-      <ul class="tab-group">
-        <li class="tab"><a href="#signup">Sign Up</a></li>
-        <li class="tab active"><a href="#login">Log In</a></li>
-      </ul>
-
-      <div class="tab-content">
-
-         <div id="login">
-          <h1>Welcome Back!</h1>
-
-          <form action="index.php" method="post" autocomplete="off">
-
-            <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email" required autocomplete="off" name="email"/>
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password" required autocomplete="off" name="password"/>
-          </div>
-
-          <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
-
-          <button class="button button-block" name="login" />Log In</button>
-
-          </form>
-
-        </div>
-
-        <div id="signup">
-          <h1>Sign Up for Free</h1>
-
-          <form action="index.php" method="post" autocomplete="off">
-
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" name='firstname' />
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off" name='lastname' />
-            </div>
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off" name='email' />
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off" name='password'/>
-          </div>
-
-          <button type="submit" class="button button-block" name="register" />Register</button>
-
-          </form>
-
-        </div>
-
-      </div><!-- tab-content -->
-
-</div> <!-- /form -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-    <script src="js/index.js"></script>
-
-</body>
-</html>
